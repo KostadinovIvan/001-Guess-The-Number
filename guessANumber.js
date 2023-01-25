924871c886bc90d63759db77888260c3fa9d699c
Ivan Kostadinov <123491210+KostadinovIvan@users.noreply.github.com>
2023-01-25 21:56:34 +0200

    Added some code

---
+++ guessANumber.js
@@ -0,0 +1,14 @@
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let computerGuess = Math.floor(Math.random()*100)
+
+let recursiveAsyncReadLine = function () {
+    readline.question(`Guess the number (0 - 100): `, number =>
+    {
+        if ( )
+    });
+}
+recursiveAsyncReadLine();
