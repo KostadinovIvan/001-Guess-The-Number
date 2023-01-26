5866d333ff24af28f8fc62364f53afb3df70d630
Ivan Kostadinov <123491210+KostadinovIvan@users.noreply.github.com>
2023-01-26 02:19:22 +0200

    Added main logic and difficulty options

--- guessANumber.js
+++ guessANumber.js
@@ -3,12 +3,72 @@ const readline = require("readline").createInterface({
     output: process.stdout
 });
 
-let computerGuess = Math.floor(Math.random()*100)
+let CPUNum = Math.floor(Math.random() * 100)
+let shots = 0;
+let difficultyLevel = "";
 
-let recursiveAsyncReadLine = function () {
-    readline.question(`Guess the number (0 - 100): `, number =>
-    {
-        if ( )
+function setDifficulty() {
+    readline.question("Choose difficulty: [E]asy, [M]edium, [H]ard: ", str => {
+        difficultyLevel = str.toLowerCase();
+
+        switch (difficultyLevel) {
+            case "e":
+            case "easy":
+                shots = 20;
+                difficultyLevel = "Easy";
+                guessNum();
+                break;
+            case "m":
+            case "medium":
+                shots = 10;
+                difficultyLevel = "Medium";
+                guessNum();
+                break;
+            case "h":
+            case "hard":
+                shots = 5;
+                difficultyLevel = "Hard";
+                guessNum();
+                break;
+            default:
+                console.log("Invalid input! Try again");
+                setDifficulty();
+                break;
+        }
     });
 }
-recursiveAsyncReadLine();
+setDifficulty();
+
+function guessNum() {
+    if (shots > 0) {
+        console.log(`${difficultyLevel} mode. You have ${shots} attempts!`);
+        console.log("---------------------------");
+
+        readline.question(`Guess the number (0 - 100): `, number => {
+            let guess = Number(number);
+            
+            if (guess <= 100 && guess >= 0) {
+                if (guess === CPUNum) {
+                    console.log("You guess it!");
+                    return readline.close();
+                } else if (guess > CPUNum) {
+                    console.log("---------------------------");
+                    console.log("Too High!");
+                    shots--
+                    guessNum();
+                } else if (guess < CPUNum) {
+                    console.log("---------------------------");
+                    console.log("Too Low!");
+                    shots--
+                    guessNum();
+                }
+            } else {
+                console.log("Invalid input! Try again");
+                guessNum();
+            }
+        });
+    } else {
+        console.log(`My number was ${CPUNum}. You loose! Good luck next time!`);
+        return readline.close();
+    }
+}
